dc48d6701ec7d1f4b621a2f29ad3ef8ef46eeca8
JinHo Do <greatdjh@gmail.com>
2019-09-30 23:47:14 +0900

    Add hooks at PostCard

--- frontend/components/PostCard/PostCard.js
+++ frontend/components/PostCard/PostCard.js
@@ -1,24 +1,88 @@
+import { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Icon, Avatar, Button } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Card, Icon, Avatar, Button, List, Comment, Input } from 'antd';
+
+import { ADD_COMMENT_REQUEST } from '../../reducers/post';
 
 const PostCard = ({ post }) => {
+  const dispatch = useDispatch();
+  const { me } = useSelector(state => state.user);
+
+  const [isCommentFormOpen, setCommentFormOpen] = useState(false);
+  const [commentText, setCommentText] = useState('');
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpen(prevState => !prevState);
+  }, []);
+
+  const onSubmitComment = useCallback(e => {
+    e.preventDefault();
+
+    if (!me) {
+      return alert('로그인 한 유저만 댓글을 작성할 수 있습니다.');
+    }
+
+    return dispatch({
+      type: ADD_COMMENT_REQUEST,
+    });
+  }, []);
+
+  const onChangeCommentText = useCallback(e => {
+    setCommentText(e.target.value);
+  }, []);
+
   return (
-    <Card
-      cover={post.img && <img src={post.img} alt="cover" />}
-      actions={[
-        <Icon type="retweet" key="retweet" />,
-        <Icon type="heart" key="heart" />,
-        <Icon type="message" key="message" />,
-        <Icon type="ellipsis" key="ellipsis" />,
-      ]}
-      extra={<Button>팔로우</Button>}
-    >
-      <Card.Meta
-        avatar={<Avatar>{post.user.nickname[0]}</Avatar>}
-        title={post.user.nickname}
-        description={post.content}
-      />
-    </Card>
+    <>
+      <Card
+        cover={post.img && <img src={post.img} alt="cover" />}
+        actions={[
+          <Icon type="retweet" key="retweet" />,
+          <Icon type="heart" key="heart" />,
+          <Icon type="message" key="message" onClick={onToggleComment} />,
+          <Icon type="ellipsis" key="ellipsis" />,
+        ]}
+        extra={<Button>팔로우</Button>}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.user.nickname[0]}</Avatar>}
+          title={post.user.nickname}
+          description={post.content}
+        />
+      </Card>
+
+      {isCommentFormOpen && (
+        <>
+          <Form onSubmit={onSubmitComment}>
+            <Form.Item>
+              <Input.TextArea
+                rows={4}
+                value={commentText}
+                onChange={onChangeCommentText}
+              />
+            </Form.Item>
+            <Button type="primary" htmlType="submit">
+              삐약
+            </Button>
+          </Form>
+          <List
+            header={`${post.comment ? post.comment.length : 0} 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.comment || []}
+            renderItem={item => (
+              <li>
+                <Comment
+                  author={item.user.nickname}
+                  avatar={<Avatar>{item.user.nickname[0]}</Avatar>}
+                  content={item.content}
+                  dateTime={item.createdAt}
+                />
+              </li>
+            )}
+          />
+        </>
+      )}
+    </>
   );
 };
 
